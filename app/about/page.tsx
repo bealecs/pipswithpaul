d11461aacd37c3c8a26e6d0aad6a7c099ac5ed8c
bealecs <clifcodes@gmail.com>
2023-02-05 17:48:42 -0600

    n.a

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,8 @@ import { Avatar, Box, Stack, Text, useColorModeValue } from '@chakra-ui/react';
 export default function About() {
   return (
     <>
-    <Features />
+
+        <Features />
     <Stack
       bg={useColorModeValue('gray.50', 'gray.800')}
       py={16}
